ae4211553767ff09f2e6aa35d84b2128610ec71c
Tadeu Valentt <tadeu.valentt@gmail.com>
2017-05-06 16:11:46 -0300

    Implement new theme
    Add new revised theme

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  greetings();
+});
+
+function greetings() {
+  console.log("%cHey there! \uD83D\uDE1C I love to inspect codes too \ud83d\udc40", "font-size: 16px; color: #ff0f00");
+  console.log("%cI'm sure we can do great things together. Find me at! window.atob('dGFkZXUudmFsZW50dEBnbWFpbC5jb20=');", "font-size: 16px; color: #5d1eb2");
+}
